e286ddce6c6d43911ac1f2fe70eec4bea22db6d3
leechaehyeon30 <dlcogus13@icloud.com>
2024-11-10 02:55:20 +0900

    랜딩페이지 11/9

--- script.js
+++ script.js
@@ -12,86 +12,18 @@ $(document).ready(function() {
         $('.project1').css('transform', 'translateX(0)');
     });
     $('.project3').mouseenter(function() {
-        $('.project1, .project2, .hidden_num2').css('transform', 'translateX(-3.5vw)');
+        $('.project1, .project2').css('transform', 'translateX(-3.5vw)');
     });
     $('.project3').mouseleave(function() {
-        $('.project1, .project2, .hidden_num2').css('transform', 'translateX(0)');
+        $('.project1, .project2').css('transform', 'translateX(0)');
     });
     $('.project4').mouseenter(function() {
-        $('.project1, .project2, .hidden_num2, .project3, .hidden_num3').css('transform', 'translateX(-3.5vw)');
+        $('.project1, .project2, .project3').css('transform', 'translateX(-3.5vw)');
     });
     $('.project4').mouseleave(function() {
-        $('.project1, .project2, .hidden_num2, .project3, .hidden_num3').css('transform', 'translateX(0)');
+        $('.project1, .project2, .project3').css('transform', 'translateX(0)');
     });
 
-    // .project1에 마우스 올렸을 때 넘버이동
-    $('.project1').hover(
-        function() {
-            $('.hidden_num1').css({
-                'left': '11.5%',
-                'bottom': '-15.3%',
-                'font-size': '25rem',
-            });
-        },
-        function() {
-            $('.hidden_num1').css({
-                'left': '26%',
-                'bottom': '-13.5%',
-                'font-size': '22rem'
-            });
-        }
-    );
-    // .project2에 마우스 올렸을 때 넘버이동
-    $('.project2').hover(
-        function() {
-            $('.hidden_num2').css({
-                'left': '26.5%',
-                'bottom': '-15.3%',
-                'font-size': '25rem',
-            });
-        },
-        function() {
-            $('.hidden_num2').css({
-                'left': '45%',
-                'bottom': '-13.5%',
-                'font-size': '22rem'
-            });
-        }
-    );
-    // .project3에 마우스 올렸을 때 넘버이동
-    $('.project3').hover(
-        function() {
-            $('.hidden_num3').css({
-                'left': '44.5%',
-                'bottom': '-15.3%',
-                'font-size': '25rem',
-            });
-        },
-        function() {
-            $('.hidden_num3').css({
-                'left': '63%',
-                'bottom': '-13.5%',
-                'font-size': '22rem'
-            });
-        }
-    );
-    // .project4에 마우스 올렸을 때 넘버이동
-    $('.project4').hover(
-        function() {
-            $('.hidden_num4').css({
-                'left': '62%',
-                'bottom': '-15.3%',
-                'font-size': '25rem',
-            });
-        },
-        function() {
-            $('.hidden_num4').css({
-                'left': '81%',
-                'bottom': '-13.5%',
-                'font-size': '22rem'
-            });
-        }
-    );
 
 
     // .project1에 마우스를 올렸을 때 요소들 보이게 만들기
