d098d342faf30c4837c230f70cae7506dedfd029
leechaehyeon30 <dlcogus13@icloud.com>
2024-11-13 20:51:54 +0900

    11/13 수정

--- script.js
+++ script.js
@@ -70,4 +70,69 @@ $(document).ready(function() {
             $('.sec_txt4').stop(true, true).fadeOut(100);
         }
     );
+
+    // 메인에서 노매뉴얼로 이동 
+    $('#nextproject').click(function(e) {
+        e.preventDefault();  // 기본 링크 동작 막기
+    
+        $('.project_nomanual').animate({
+            'left': '0%'  // 화면에 보이도록 0% 위치로 이동
+        }, 1000);
+        $('.project_nomanual').show();
+    
+        $('.main').animate({
+            'left': '-100%'  // 왼쪽으로 슬라이드
+        }, 1000, function(){
+            $('.main').hide();
+        });
+    });
+    
+    $('#backBtn').click(function() {
+        $('.project_nomanual').animate({
+            'left': '100%'  // 오른쪽으로 100% 이동
+        }, 1000, function() {
+            $('.project_nomanual').hide();
+        });
+    
+        $('.main').animate({
+            'left': '0%'  // 원래 위치로 돌아옴
+        }, 1000);
+        $('.main').show();
+
+    });
+    
+    // 노매뉴얼 사이트 이동
+    $('#viewSiteBtn').click(function() {
+        // 새 탭에서 링크 열기
+        window.open("https://leechaehyeon30.github.io/nomanual/", "_blank");
+    });
+
+
+
+    var scrollInterval; // 스크롤 인터벌 저장 변수
+    
+    $('#mac_ver').hover(function() {
+        // 호버했을 때 자동 스크롤 시작
+        var iframe = $('#mac_ver')[0];  // iframe DOM 객체
+        var iframeDoc = iframe.contentWindow.document; // iframe 내부 문서
+
+        var scrollAmount = 0; // 스크롤 양
+        scrollInterval = setInterval(function() {
+            // 스크롤 위치를 설정
+            iframeDoc.body.scrollTop = scrollAmount; 
+            iframeDoc.documentElement.scrollTop = scrollAmount; // HTML 문서의 스크롤 위치도 변경
+
+            scrollAmount += 10; // 스크롤 양을 조금씩 증가시킴
+            if (scrollAmount > iframeDoc.body.scrollHeight - iframe.clientHeight) {
+                clearInterval(scrollInterval); // 끝까지 스크롤되면 멈춤
+            }
+        }, 50); // 50ms마다 스크롤 이동
+    }, function() {
+        // 마우스가 떠났을 때 자동 스크롤 멈춤
+        clearInterval(scrollInterval); // 자동 스크롤을 멈춤
+
+        iframeDoc.body.scrollTop = 0;
+        iframeDoc.documentElement.scrollTop = 0;
+    });
+    
 });
